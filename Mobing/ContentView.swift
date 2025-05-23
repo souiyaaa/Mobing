087aa221f597daaf06c6fe661cc633c137e6efcf
Daffa954 <dkhoirul05@gmail.com>
2025-05-23 17:16:21 +0700

    Initial commit

---
+++ Mobing/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Mobing
+//
+//  Created by Daffa Khoirul on 23/05/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Mobing/MobingApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobingApp.swift
+//  Mobing
+//
+//  Created by Daffa Khoirul on 23/05/25.
+//
+
+import SwiftUI
+
+@main
+struct MobingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
